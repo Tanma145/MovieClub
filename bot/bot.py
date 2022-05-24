4343e9608e5e7fbc945d6a20e7bd0843335b3299
Egor Mikheev <tanma145@yandex.ru>
2022-05-25 01:42:01 +0300

    my first commit lmao

---
+++ bot/bot.py
@@ -0,0 +1,4 @@
+class Bot:
+    def __init__(self, key, db_url):
+        self.key = key
+        self.db_url = db_url
--- main.py
+++ main.py
@@ -1,2 +1,19 @@
+import hikari
+import os
+from dotenv import load_dotenv
+
+try:
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+    if os.path.exists(dotenv_path):
+        load_dotenv(dotenv_path)
+        # keys
+        API_KEY = os.environ.get("API_KEY")
+        DATABASE_URL = os.environ.get("DATABASE_URL")
+        OOF = os.environ.get("OOF")
+        if None in (API_KEY, DATABASE_URL, OOF):
+            raise Exception
+except Exception as e:
+    print("oof")
+
 if __name__ == '__main__':
     print('hello!')
