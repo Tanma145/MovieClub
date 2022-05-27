e7fd3492f983b4bb0809cf912f322fcf7ef88c14
k-t-l-h <laciedreamer@gmail.com>
2022-05-28 01:06:11 +0300

    add emoji listeners

--- bot/bot.py
+++ bot/bot.py
@@ -1,7 +1,7 @@
 import hikari
 import re
 
-bot = hikari.GatewayBot(token='')
+bot = hikari.GatewayBot(token='OTc4NzU4ODg3ODc0MzYzNDQ2.GAfkl-.kkrRL2yCqjj6mh_-15WTowPtn8pR4xNW5KGDdI')
 
 
 @bot.listen(hikari.MessageCreateEvent)
@@ -15,5 +15,16 @@ async def suggestion_wall(event: hikari.GuildMessageCreateEvent):
     find = re.search(r'(?P<film_name>.+?)(.)?(?P<film_url>(?:http|https).*)', event.content)
     name = find.group('film_name')
     url = find.group('film_url')
+    id = event.message.id
 
-    print('Film info: {0} {1}'.format(name, url))
+    print('Film info: {0} {1} {2}'.format(name, url, id))
+
+
+@bot.listen(hikari.ReactionAddEvent)
+async def suggestion_wall(event: hikari.ReactionAddEvent):
+    print('mark added')
+
+
+@bot.listen(hikari.ReactionDeleteEvent)
+async def suggestion_wall(event: hikari.ReactionDeleteEvent):
+    print('mark deleted')
